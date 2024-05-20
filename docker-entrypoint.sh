4807f1d6f9d1c93987783b1aa5816fe7a74d6929
Mannes Brak <info@mbtech.nl>
2024-05-20 12:55:10 +0200

    update to use docker compose

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -37,30 +37,26 @@ if [ -z "$INPUT_SSH_PORT" ]; then
 fi
 
 STACK_FILE=${INPUT_STACK_FILE_NAME}
-DEPLOYMENT_COMMAND_OPTIONS="--host ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_SSH_PORT"
+DEPLOYMENT_COMMAND_OPTIONS=""
 
-DEPLOYMENT_COMMAND="docker-compose -f $STACK_FILE"
-
+DEPLOYMENT_COMMAND="docker compose -f $STACK_FILE"
 
 SSH_HOST=${INPUT_REMOTE_DOCKER_HOST#*@}
+#DOCKER_HOST="ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_SSH_PORT"
 
 echo "Registering SSH keys..."
-
 # register the private key with the agent.
 mkdir -p ~/.ssh
 ls ~/.ssh
 printf '%s\n' "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-printf '%s\n' "$INPUT_SSH_PUBLIC_KEY" > ~/.ssh/id_rsa.pub
-chmod 600 ~/.ssh/id_rsa.pub
-#chmod 600 "~/.ssh"
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_rsa
 
 
 echo "Add known hosts"
-ssh-keyscan -p $INPUT_SSH_PORT "$SSH_HOST" >> ~/.ssh/known_hosts
-ssh-keyscan -p $INPUT_SSH_PORT "$SSH_HOST" >> /etc/ssh/ssh_known_hosts
+printf '%s\n' "$INPUT_SSH_PUBLIC_KEY" > ~/.ssh/id_rsa
+printf '%s\n' "$INPUT_SSH_PUBLIC_KEY" > /etc/ssh/ssh_known_hosts
 # set context
 echo "Create docker context"
 docker context create staging --docker "host=ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_SSH_PORT"
@@ -77,5 +73,3 @@ ${DEPLOYMENT_COMMAND} ${DEPLOYMENT_COMMAND_OPTIONS} pull
 
 echo "Command: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS}"
 ${DEPLOYMENT_COMMAND} ${DEPLOYMENT_COMMAND_OPTIONS} ${INPUT_ARGS}
-
-
